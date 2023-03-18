73118227c82c5ca4734dc3832de0d1488d9055bc
Artem Kocharov <82714240+ArtemSchmel@users.noreply.github.com>
2023-03-18 18:09:00 +0400

    commit

---
+++ ConnectPackage/ConnectPackage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using PowerCollections;
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 1; i < Capacity; i++)
+{
+    stack.Push(i);
+}
+
+Console.WriteLine("Стек содержит:");
+
+foreach (int num in stack)
+{
+    Console.WriteLine(num);
+}
