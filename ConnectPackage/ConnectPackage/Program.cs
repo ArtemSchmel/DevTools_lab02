6c476aeaf9b0e4011485d978a1090762b9f8a2d8
Artem Kocharov <82714240+ArtemSchmel@users.noreply.github.com>
2023-03-19 13:08:21 +0400

    2commit

--- ConnectPackage/ConnectPackage/Program.cs
+++ ConnectPackage/ConnectPackage/Program.cs
@@ -2,16 +2,41 @@
 using System.Collections;
 using PowerCollections;
 
-Stack<int> stack = new Stack<int>();
-
-for (int i = 1; i < Capacity; i++)
+namespace StackExample
 {
-    stack.Push(i);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 1; i < Capacity; i++)
+            {
+                stack.Push(i);
+            }
+
+            Console.WriteLine("Стек содержит:");
+
+            foreach (int num in stack)
+            {
+                Console.WriteLine(num);
+            }
+
+            Stack stack2 = new Stack();
+            Random random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int number = random.Next(0, 101);
+                stack2.Push(number);
+            }
+
+            Console.WriteLine("Стек содержит:");
+            foreach (var item in stack2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
 }
 
-Console.WriteLine("Стек содержит:");
-
-foreach (int num in stack)
-{
-    Console.WriteLine(num);
-}
